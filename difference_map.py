d55d9d5262ebf5ecc781f6e8b85dc9f0b0c09280
u0167812 <hugo.li@kuleuven.be>
2025-08-11 10:39:23 +0200

    difference map

--- difference_map.py
+++ difference_map.py
@@ -9,7 +9,6 @@ from torch.utils.data import DataLoader, random_split
 
 
 def main(model_dir, healthy_dir, defective_dir, max_size=6, val_split=0.1):
-
     # load VAE
     vae, random_state = load_vae(model_dir, 'cpu', with_rand_state=True)
     vae.eval()
@@ -19,9 +18,9 @@ def main(model_dir, healthy_dir, defective_dir, max_size=6, val_split=0.1):
     dataset = MedicalImageDataset(healthy_dir, defective_dir)
     generator = torch.Generator().manual_seed(random_state)
     _, val_dataset = random_split(dataset, [1 - val_split, val_split], generator=generator)
-    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=True, pin_memory=True)
 
-    fig, axes = plt.subplots(nrows=max_size, ncols=4, figsize=(12, 15))
+    fig, axes = plt.subplots(nrows=max_size, ncols=6, figsize=(12, 15))
     num_healthy=0
     num_defective=0
     idx = 0
@@ -54,11 +53,19 @@ def main(model_dir, healthy_dir, defective_dir, max_size=6, val_split=0.1):
             ax.axis('off')
 
             ax = axes[idx - 1, 3]
-            im = ax.imshow(img_1[64,:, :].T - img_0[64,:, :].T,cmap='Spectral_r', vmin=-1, vmax=1, origin='lower')
+            ax.imshow(img_1[64,:, :].T - img_0[64,:, :].T,cmap='Spectral_r', vmin=-1, vmax=1, origin='lower')
+            ax.axis('off')
+
+            ax = axes[idx - 1, 4]
+            ax.imshow(img_1[:, :, 64].T, cmap='gray', origin='lower')
+            ax.axis('off')
+
+            ax = axes[idx - 1, 5]
+            im = ax.imshow(img_1[:, :, 64].T - img_0[:, :, 64].T, cmap='Spectral_r', vmin=-1, vmax=1, origin='lower')
             ax.axis('off')
 
             if idx >= max_size:
-                cbar_ax = fig.add_axes([0.2, 0.05, 0.9, 0.03])  # [left, bottom, width, height]
+                cbar_ax = fig.add_axes([0.1, 0.05, 0.8, 0.03])  # [left, bottom, width, height]
                 fig.colorbar(im, cax=cbar_ax, orientation='horizontal')
 
                 plt.tight_layout(rect=[0, 0.1, 1, 1])  # Leave space for colorbar
