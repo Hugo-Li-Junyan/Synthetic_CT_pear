d13eee0f22863df25c4d1e6c83b7fe2145c7eb5d
u0167812 <hugo.li@kuleuven.be>
2026-07-14 16:21:29 +0200

    augmentation strategy

--- train_clf_3d.py
+++ train_clf_3d.py
@@ -118,7 +118,7 @@ def build_transform(augment):
     return tio.Compose([
         tio.RandomFlip(axes=(0, 1)),
         tio.RandomAffine(scales=(0.9, 1.1), degrees=(0, 0, 0, 0, -30, 30), isotropic=True),
-        tio.RandomGamma(log_gamma=(-0.1, 0.1)),
+        tio.RandomGamma(log_gamma=(-0.2, 0.2)),
     ])
 
 
